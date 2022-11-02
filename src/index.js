eb420660266b0edb1b6a6d4c95ffd503fa36396c
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-11-02 21:38:01 +0100

    feat: add clear button and add css style

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ liste=liste.sort((a,b) =>  a.index - b.index );
     document.getElementById("todolist").innerHTML+=`
     <li class="task" id="${element.index}">
       <span>
-      <input type="checkbox" id="check${element.index}" class=${element.completed? "inputChecked" : ""}>
+      <input type="checkbox" id="check${element.index}" class=${element.completed? "inputChecked" : ""} ${element.completed? "checked" : ""}>
       <label for="check${element.index}" class=${element.completed? "labelChecked" : ""}>${element.description}</label>
       </span>
       <span class="hamburger" id="button${element.index}">&#8942;</span>
@@ -22,6 +22,9 @@ liste=liste.sort((a,b) =>  a.index - b.index );
     `;
 
   });
+  document.getElementById("todolist").innerHTML+=`
+  <button type="button">Clear all completed</button>
+  `;
 };
 window.addEventListener('load', () => {
 const tasks=[{ 'description': 'wash the dishes', 'completed':false, 'index': 0},
