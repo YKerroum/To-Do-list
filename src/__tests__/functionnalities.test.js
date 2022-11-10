ed93be99a12dbf03a983df8c657c41db26083a0e
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-11-10 17:52:25 +0100

    feat: add test to edit functionality

--- src/__tests__/functionnalities.test.js
+++ src/__tests__/functionnalities.test.js
@@ -16,6 +16,22 @@ describe('add to List', () => {
   });
 });
 
+describe('edit a task description', () =>{
+  test('edit the description of a task from "Task1" to "Exemple1" and check if it changes in localstorage',()=>{
+    //Arrange
+    const id=1;
+    const description ='Exemple1';
+
+    // Act
+    tasks.editTask(id, description);
+
+    // Assert
+    expect((localStorage).getItem('tasks')).toBe(JSON.stringify([{ index: 1, description: 'Exemple1', completed: true }]));
+  
+  })
+
+})
+
 describe('Remove from list', () => {
   test("remove the only one task from the list and check if it's length is equal to 0", () => {
     // Arrange
@@ -26,3 +42,4 @@ describe('Remove from list', () => {
     expect(localStorage.getItem('tasks')).toBe('[]');
   });
 });
+
